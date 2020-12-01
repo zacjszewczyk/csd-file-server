82b2d2983be6ffa2d37ab6d87d3ce1dc41b14c49
Zachary Szewczyk <zjszewczyk@student.ysu.edu>
2020-12-01 15:05:20 -0500

    Initial commit.

---
+++ client/client.py
@@ -0,0 +1,211 @@
+#!/usr/bin/python3
+
+# Imports
+from argparse import ArgumentParser, ArgumentTypeError # CLI argument parsing
+from re import match # Regex IP input validation
+from os.path import exists # Validate files to send/receive
+import socket
+
+# Constants
+CHUNKSIZE = 1024
+ENCODING = "utf-8"
+
+# Method: ip
+# Purpose: Validate that the user entered a string containing four groups of
+# 1 to 3 digits each as the server's IP address.
+# Parameters:
+# - arg: User-submitted server IP address.
+# Return:
+# - Success: User-submitted server IP address. (String)
+# - Failure: Error raised and execution halted. (ArgumentTypeError)
+def ip(arg):
+    # Transform "localhost" into a valid IP address.
+    if (arg == "localhost"): arg = "127.0.0.1"
+    # Validate user-submitted server IP address.
+    if not match(r"(\d{1,3}\.){3}\d{1,3}",arg): raise ArgumentTypeError
+    return arg
+
+class conn:
+    def __init__(self, server=None, server_port=None):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = server
+        self.server_port = server_port
+
+    def connect(self, server=None, server_port=None):
+        # Ensure that the user has, either when creating a new "conn" object
+        # or when calling this function, provided a server IP address. Store
+        # that server IP address in the self.server variable.
+        if (self.server == None and server == None):
+            raise Exception("Error: No server specified. Please try again.")
+        self.server = self.server if self.server != None else server
+        
+        # Ensure that the user has, either when creating a new "conn" object
+        # or when calling this function, provided a server port. Store that
+        # server port in the self.server_port variable.
+        if (self.server_port == None and server_port == None):
+            raise Exception("Error: No server port specified. Please try again.")
+        self.server_port = self.server_port if self.server_port != None else server_port
+        
+        # Connect to the server at self.server on port self.server_port
+        self.sock.connect((self.server, self.server_port))
+
+    def close(self):
+        self.sock.shutdown(socket.SHUT_RDWR)
+        self.sock.close()
+
+    def send_file(self, filename):
+        fd = open(filename, "rb")
+        while True:
+            chunk = fd.read(CHUNKSIZE)
+            self._send(chunk)
+            if (len(chunk) < CHUNKSIZE): break
+        fd.close()
+        del fd
+
+    def send_msg(self, msg):
+        b = bytes(msg, encoding="utf-8")
+        for chunk in (b[i:i+CHUNKSIZE] for i in range(0, len(b), CHUNKSIZE)):
+            self._send(chunk)
+        del b
+
+    def _send(self, buff):
+        self.sock.send(buff)
+    
+    def save_as(self, filename):
+        fd = open(filename, "wb")
+        fd.write(self._recv())
+        fd.close()
+        return 0
+
+    def get_msg(self):
+        return self._recv().decode("utf-8")
+
+    def _recv(self):
+        buff = []
+        while True:
+            buff.append(self.sock.recv(CHUNKSIZE))
+            if (len(buff[-1]) < 1024): break
+        return b"".join(buff)
+
+
+    # def myreceive(self):
+    #     chunks = []
+    #     bytes_recd = 0
+    #     while bytes_recd < MSGLEN:
+    #         chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+    #         if chunk == b'':
+    #             raise RuntimeError("socket connection broken")
+    #         chunks.append(chunk)
+    #         bytes_recd = bytes_recd + len(chunk)
+    #     return b''.join(chunks)
+
+# If run as a standalone program, parse the supplied arguments and act
+# accordingly; if this script is imported into another, this section will
+# not execute.
+if (__name__ == "__main__"):
+    # Define an instance of the built-in command-line interface argument parser.
+    parser = ArgumentParser()
+
+    # Add flags.
+    ## Add optional flag for controlling output verbosity, "-v" or "--verbose".
+    parser.add_argument("-v", "--verbose", help="Verbose output", action="store_true", dest="verbose")
+
+    ## Add required flags.
+    ### a. An argument indicates the server to connect to. (-s or --server)
+    parser.add_argument("-s", "--server", help="Server IP address", type=ip, required=True, dest="server")
+    ### b. An argument indicates the port to attempt to connect to the server over.
+    ###    (-p or --port)
+    parser.add_argument("-p", "--port", help="Server port", type=int, required=True, dest="server_port")
+    ### c. An argument indicates whether or not the client is uploading a file or
+    ###    downloading a file. (-u or --upload for uploading,
+    ###    or -d or --download for downloading). Only one is accepted; entering 
+    ###    both will generate an error and halt execution.
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-u", "--upload", help="Upload flag", action="store_const", dest="action", const="UPLOAD")
+    group.add_argument("-d", "--download", help="Download flag", action="store_const", dest="action", const="DOWNLOAD")
+    ### d. An argument that indicates the name of the file that will be uploaded or requested for download.
+    parser.add_argument("-f", "--file", help="Target file name", type=str, required=True, dest="filename")
+
+    # Parse command-line arguments.
+    args = parser.parse_args()
+
+    # Perform basic bounds checking
+    ## Validate all server IP address octets are within the valid 0-255 range.
+    ## Produce an error and then exit if not.
+    for octet in args.server.strip().split("."):
+        if (int(octet) < 0 or int(octet) > 255):
+            raise Exception("Error: Invalid server IP address. Please try again.")
+    ## Validate server port is within the valid range 0-65535. Produce an error
+    ## and then exit if not.
+    if (args.server_port < 0 or args.server_port > 65535):
+        raise Exception("Error: Invalid server port specified. Please try again.")
+
+    # Create a new connection object c for the server connection, then connect.
+    c = conn(args.server, args.server_port)
+    c.connect()
+
+    # Handle file uploading.
+    if (args.action == "UPLOAD"):
+        # Verify that the file to be uploaded exists on the local system.
+        # Produce an error and then exit if it does not.
+        if not (exists(args.filename)):
+            raise Exception("Error: File to upload does not exist.")
+
+        # Verbose output
+        if (args.verbose): print(f"Uploading file {args.filename} ... ", end="", flush=True)
+
+        # Notify the server of a pending file upload.
+        c.send_msg(args.action)
+        
+        # If the server is ready for the file upload, first send the filename,
+        # then upload the file.
+        if (c.get_msg() == "READY"):
+            c.send_msg(f"SENDING {args.filename}")
+            c.send_file(args.filename)
+        # If the server is not ready for the file upload, print an error
+        # message for the user and prompt them to retry.
+        else:
+            print("Error: Server not ready to accept file transfer. Please try again.")
+            exit(0)
+
+        # Verbose output
+        if (args.verbose): print("done")
+
+    elif (args.action == "DOWNLOAD"):
+        # Verify that the file to be downloaded does not already exist. Produce
+        # an error and then exit if downloading the specified file would
+        # overwrite an existing one.
+        if (exists(args.filename)):
+            raise Exception("Error: File to download already exists.")
+        
+        # Request a file download from the server.
+        c.send_msg(args.action)
+
+        # If the server is ready for the file download, send the filename.
+        if (c.get_msg() == "READY"):
+            c.send_msg(f"DOWNLOAD {args.filename}")
+            # If the server responde with "SENDING", receive the file and save
+            # it with the filename supplied by the user.
+            resp  = c.get_msg()
+            if ("SENDING" in resp):
+                # Verbose output
+                if (args.verbose): print(f"Downloading file {args.filename} with {resp.split(' ',1)[1]} bytes ... ", end="", flush=True)
+                c.save_as(args.filename)
+                print("done.")
+            # If the server responds with "FILE_NOT_FOUND", the file does not
+            # exist.
+            else:
+                print("Error: Server cannot find file. Please try again.")
+                exit(0)    
+        else:
+            print("Error: Server not ready to transfer file. Please try again.")
+            exit(0)
+    # Account for an edge case where the client provides an unspecified action.
+    else:
+        raise Exception("Invalid action specified. Please try again.")
+
+    # Close the connection to the server once the upload or download operation
+    # has completed. Account for the fact that the server may close the
+    # connection first.
+    try: c.close()
+    except: pass
---
+++ server/server.c
@@ -0,0 +1,232 @@
+// Includes
+#include <netinet/in.h> // Build socket
+#include <stdio.h> // Write to console
+#include <unistd.h> // Read data from socket
+#include <stdlib.h> // Exit on error
+#include <string.h> // Compare strings
+#include <sys/stat.h> // Validate files and directories
+
+// Main function
+int main(int argc, char const *argv[]) 
+{ 
+	// Perform basic input validation.
+	// Validate that the user entered two command-line arguments
+	if (argc != 5) {
+		printf("Error: Server port and target directory not specified. Please try again.\n");
+		exit(1);
+	}
+	// Extract and validate server port
+	int port_location = 0;
+	if (strstr(argv[1],"-p")) {
+		port_location = 2;
+	} else if (strstr(argv[3],"-p")) {
+		port_location = 4;
+	} else {
+		printf("Error: Server port not specified. Please try again.\n");
+		exit(1);
+	}
+	int const server_port = atoi(argv[port_location]);
+	if (server_port < 0 || server_port > 65535) {
+		printf("Error: Invalid server port. Please try again.\n");
+		exit(1);	
+	}
+
+	// Extract and validate target directory
+	int dir_location = 0;
+	if (strstr(argv[1],"-d")) {
+		dir_location = 2;
+		printf("Directory first\n");
+	} else if (strstr(argv[3],"-d")) {
+		dir_location = 4;
+	} else {
+		printf("Error: Target directory not specified. Please try again.\n");
+		exit(1);
+	}
+	const char* dir = argv[dir_location];
+	struct stat st;
+	if (stat(dir, &st) != 0 || S_ISDIR(st.st_mode) == 0) {
+        printf("Error: Target directory does not exist. Please try again.\n");
+		exit(1);
+    }
+
+	// Initialize the sockets
+	int server_sock; // Server socket
+	int client_sock; // Client socket
+	// Attempt to create the server socket. Print an error on failure and halt
+	// execution.
+	if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
+		printf("Error: Socket creation failed."); 
+		exit(1); 
+	}
+	
+	// Construct the server address object to be bound to the server socket.
+	struct sockaddr_in address; // 
+	address.sin_addr.s_addr = INADDR_ANY; // Accept any source IP addresses.
+	address.sin_family = AF_INET; // Required.
+	address.sin_port = htons(server_port); // Set server port.
+	int addrlen = sizeof(address); // Size of the address object for binding.
+	int opt = 1; // setsockopt() optval field.
+	
+	// Configure the socket options. Print an error on failure and halt
+	// execution.
+	if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) { 
+		printf("Error: Socket configuration failed."); 
+		exit(1); 
+	} 
+
+	// Bind the socket. Print an error on failure and halt execution.
+	if (bind(server_sock, (struct sockaddr *)&address, sizeof(address)) < 0) { 
+		printf("Error: Socket binding failed."); 
+		exit(1);
+	} 
+
+	// Start listening for connections on the server socket.
+	if (listen(server_sock, 3) < 0) {
+		printf("Error: Failed to listen on server port %d\n",server_port);
+		exit(1); 
+	}
+
+	// Print a status message to the user, so that they know that the server
+	// is now listening for connections.
+	printf("Listening on port %d.\n",server_port);
+
+	// Initialize a 1kb "buff" variable to store chunks of data during
+	// transmission, a "bytes_transferred" variable to track those transfers,
+	// and an "fd" file descriptor variable for sending and receiving files.
+	char buff[1024] = {0}; 
+	int bytes_transferred = 0;
+	FILE *fd;
+
+	// Accept new connections indefinitely with an infinite for loop.
+	for (;;) {
+		// Accept a new client connection; assign these sockets to "client_socket".
+		client_sock = accept(server_sock, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+		// Proceed once a client has connected.
+
+		// Read the client's command into "buff"
+		bytes_transferred = read(client_sock, buff, 1024);
+
+		// Handle a file upload.
+		if (strstr(buff,"UPLOAD") != NULL) {
+			memset(buff, 0, sizeof buff); // Clear "buff".
+			
+			// Notify the client that the server is ready to receive the file.
+			send(client_sock , "READY", 5, 0);
+
+			// Read the file's name from the "SENDING X" response, where "X"
+			// is the file's name.
+			bytes_transferred = read(client_sock, buff, 1024);
+
+			// Extract the filename from the message and store it in the
+			// variable "fn." Concatenate this with the path provided by the
+			// user to produce "filename".
+			bytes_transferred = read(client_sock, buff, 1024);
+			char *fn = strtok(buff, " ");
+			fn = strtok(NULL, " ");
+			char filename[128];
+			strcpy(filename, dir);
+			strcat(filename, fn);
+
+			// Print a status message to the user.
+			printf("Receiving file %s ... ",filename);
+			fflush(stdout);
+
+			// Clear the destination file, then open it in "append" mode so
+			// that its contents from the client can be written in chunks.
+			fd = fopen(filename,"w");
+			fclose(fd);
+			fd = fopen(filename,"a");
+
+			// Continue reading data from the client socket in 1024 byte chunks
+			// until the client stops sending data.
+			while ((bytes_transferred = read(client_sock, buff, 1024)) != 0) {
+				// Write the contents of "buff" to the output file "fd".
+				fwrite(buff, sizeof(buff[0]), sizeof(buff)/sizeof(buff[0]), fd);
+				// Clear buff in preparation for the next chunk.
+				memset(buff, 0, sizeof buff);
+			}
+
+			// Close the output file, then update the status message to the
+			// user that the file has been transferred.
+			fclose(fd);
+			printf("done.\n");
+
+		} 
+		// Handle file download.
+		else if (strstr(buff,"DOWNLOAD") != NULL) {
+			memset(buff, 0, sizeof buff); // Clear "buff".
+			
+			// Notify the client that the server is ready to send the file.
+			send(client_sock , "READY", 5, 0);
+
+			// Read the desired file's name into "fn" Concatenate this with
+			// the path provided by the user to produce "filename".
+			bytes_transferred = read(client_sock, buff, 1024);
+			char *fn = strtok(buff, " ");
+			fn = strtok(NULL, " ");
+			char filename[128];
+			strcpy(filename, dir);
+			strcat(filename, fn);
+
+			// Verify that the desired file. If it does not, send an error
+			// message "FILE_NOT_FOUND" to the client.
+			if (access(filename, F_OK) != -1) {
+				// Open the target file.
+				fd = fopen(filename,"r");
+				if (!fd) {
+					printf("Error: Failed to open file.\n");
+					exit(1);
+				}
+
+				// If the file does exist, calculate it's size, then store it
+				// in a temporary "temp" string.
+				char temp[16];
+				fseek(fd, 0L, SEEK_END);
+				int filesize = ftell(fd);
+				sprintf(temp, "%d", filesize);
+				rewind(fd);
+
+				// Prepare the client to receive the file with the message
+				// "SENDING".
+				char msg[64];
+				strcpy(msg, "SENDING ");
+				strcat(msg, temp);
+				send(client_sock, msg, strlen(msg), 0);
+
+				// Print a status message to the user.
+				printf("Sending file %s ... ",filename);
+				fflush(stdout);
+
+				// Read 1k chunks from the file at a time, then send them to
+				// the client until no more data remains.
+				memset(buff, 0, sizeof buff); // Clear "buff".
+				while ((bytes_transferred = fread(buff, 1, 1024, fd)) > 0)
+				{
+					send(client_sock, buff, bytes_transferred, 0);
+					memset(buff, 0, sizeof buff); // Clear "buff".
+				}
+
+				// Close the input file, then update the status message to the
+				// user that the file has been transferred.
+				fclose(fd);
+				printf("done.\n");
+			}
+			// If the requested file does not exist, send an error message
+			// "FILE_NOT_FOUND" to the client.
+			else {
+				send(client_sock , "FILE_NOT_FOUND", 14, 0);
+			}
+		} 
+		// Handle invalid commands.
+		else {
+			// If an invalid command was sent by the client, respond with
+			// "ERROR" and print a message to the server console.
+			send(client_sock, "ERROR", 5, 0);
+			printf("Client sent invalid command '%s'\n",buff);
+		}
+
+		// After each operation, close the connection.
+		close(client_sock);
+	}
+	return 0; // Return success
+} 
